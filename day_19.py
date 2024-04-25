30c2625749960b963a80b88f7b339967383e044d
Reed McFadden <reed.mcfadden@gmail.com>
2024-04-25 23:14:47 +0000

    day 19 part 1 complete
    Signed-off-by: Reed McFadden <reed.mcfadden@gmail.com>

--- day_19.py
+++ day_19.py
@@ -1,7 +1,32 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+def part_1():
+    lines = open('./inputs/day_19.txt', 'r').readlines()
+    medicine_molecule = lines.pop()
+    replacements_list = []
+    distinct_molecules = set()
+
+    for l in lines:
+        l = l.strip()
+        if not l:
+            continue
+        tokens = l.split(' ')
+        replacements_list.append((tokens[0], tokens[-1]))
+
+    for i in range(len(medicine_molecule)):
+        for _from, _to in replacements_list:
+            molecule_len = len(_from)
+            if _from == medicine_molecule[i:i+molecule_len]:
+                distinct_molecules.add(
+                    medicine_molecule[0:i]
+                    + _to
+                    + medicine_molecule[i+molecule_len:]
+                )
+
+    print(f'Part 1 answer: {len(distinct_molecules)}')
 
 def main():
-    print("Hello, World!")
+    part_1()
 
 if __name__ == "__main__":
     main()
